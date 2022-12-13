0f2b5786bc944ea16cb9cd9c6b49675a64a9ba1d
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2022-12-13 15:33:02 -0700

    fixed bugs so it can run, lots of unreachable code and mislabled stuff but it runs now, no views yet

--- client/src/components/shared/Team.js
+++ client/src/components/shared/Team.js
@@ -6,17 +6,22 @@ const Team = () => (
       <h3>Meet the Team</h3>
   
       <h4>Member 1</h4>
-      <img src='https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' width='100' alt='member' />
+      <img src='https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80' width='100' alt='member' />
      
-      <h4>Member 2</h4>
-      <img src='https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' width='100' alt='member' />
+      <h4>Member 1</h4>
+      <img src='https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80' width='100' alt='member' />
      
-      <h4>Member 3</h4>
-      <img src='https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' width='100' alt='member' />
+      <h4>Member 1</h4>
+      <img src='https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80' width='100' alt='member' />
      
-      <h4>Member 4</h4>
-      <img src='https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' width='100' alt='member' />
+      <h4>Member 1</h4>
+      <img src='https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80' width='100' alt='member' />
      
+      <h4>Member 1</h4>
+      <img src='https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80' width='100' alt='member' />
+     
+      <h4>Member 1</h4>
+      <img src='https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80' width='100' alt='member' />
     </>
 )
 export default Team;
