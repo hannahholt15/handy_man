6298f55e64c2ba28972b5544c5c8dc61a238d7fd
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2022-12-13 14:26:43 -0700

    nothing yet, jsut tyring to rebase

--- client/src/components/workers/Workers.js
+++ client/src/components/workers/Workers.js
@@ -1,17 +1,6 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
+const Workers = () => {
+  const [workers, setWorkers]
 
-  useEffect ( () => {
-    axios.get('/api/Worker')
-    .then( res => setWorkers(res.data))
-    .catch( err => console.log(err))
-  }, [])
-
-  const addWorker = (worker) => {
-    axios.post('/api/workers', { worker })
-      .then( res => setWorkers([...workers, res.data]))
-      .catch( err => console.log(err))
-  }
-
-} 
-export default Workers
+}
