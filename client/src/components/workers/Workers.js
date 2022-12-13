1cdba2ff700e3b1be0bc7c3ba78b10c58d4430d0
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2022-12-13 15:33:02 -0700

    fixed bugs so it can run, lots of unreachable code and mislabled stuff but it runs now, no views yet

--- client/src/components/workers/Workers.js
+++ client/src/components/workers/Workers.js
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-const Workers = () => {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Workers = () =>
 {
