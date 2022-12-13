3433e4f3b0c365a10db5065275a7c51f621ab91d
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2022-12-13 15:33:02 -0700

    fixed bugs so it can run, lots of unreachable code and mislabled stuff but it runs now, no views yet

--- client/src/components/workers/Workers.js
+++ client/src/components/workers/Workers.js
@@ -4,5 +4,11 @@ import axios from 'axios';
 const Workers = () =>
 {
 
+  const addWorker = (worker) => {
+    axios.post('/api/workers', { worker })
+      .then( res => setWorkers([...workers, res.data]))
+      .catch( err => console.log(err))
+  }
+
 } 
 export default Workers
